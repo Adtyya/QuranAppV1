0272f30c88eb37ad62bc9e6bde73eca236f8e736
aditya <aditdualapan@gmail.com>
2022-01-29 15:28:52 +0700

    update

--- components/content/index.tsx
+++ components/content/index.tsx
@@ -1,8 +1,7 @@
 import axios from "axios";
 import Link from "next/link";
 import { SyntheticEvent, useEffect, useState } from "react";
-import Skeleton from "react-loading-skeleton";
-import 'react-loading-skeleton/dist/skeleton.css';
+import Head from "next/head";
 
 export default function Content()
 {
@@ -42,65 +41,70 @@ export default function Content()
     }
 
     return(
-        <div className="container mx-auto">
-            <div className="flex item-center justify-center">
-                <form onSubmit={handleSubmit}>
-                    <input className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
-                    type="search" name="search" placeholder="Search by Surah name .." onChange={(e)=>setQuery(e.target.value)}/>
-                </form>
-            </div>
-            <div>
-                {load ? 
-                <div className="flex flex-row justify-center item-center px-3 py-2">
-                    <p>Loading data ....</p>
+        <>
+            <Head>
+                <title>Quran App</title>
+            </Head>
+            <div className="container mx-auto">
+                <div className="flex item-center justify-center">
+                    <form onSubmit={handleSubmit}>
+                        <input className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+                        type="search" name="search" placeholder="Search by Surah name .." onChange={(e)=>setQuery(e.target.value)}/>
+                    </form>
                 </div>
-                :
-                 <>
-                    <div className="flex flew-row flex-wrap w-full justify-center">
-                        {surah.filter((show: any)=>{
-                            if(query == ''){
-                                return show;
-                            }else if(show.nama_latin.toLowerCase().includes(query.replace(/\s+/g,'-').toLocaleLowerCase())){
-                                return show;
-                            }
-                        }).slice(0, page).map((show: any)=>{
-                            return(
-                                <div key={show.nomor} className="flex w-full py-3 px-3 mx-3 my-3 bg-slate-200 rounded w-full h-fit md:w-1/4 shadow shadow-lg">
-                                    <button>
-                                        <Link href={`/baca-surat-ke/${show.nomor}`} passHref>
-                                            <div>
-                                                <div className="flex flex-row hover:text-gray-500">
-                                                    <p className="text-lg rounded-full mx-4 my-3 px-2 py-2">
-                                                        {show.nomor}
-                                                    </p>
-                                                    <div className="flex-col my-3">
-                                                        <p className="font-bold italic">{show.nama_latin}</p>
-                                                        <p className="italic">{show.arti}, {show.jumlah_ayat} Ayat</p>
+                <div>
+                    {load ? 
+                    <div className="flex flex-row justify-center item-center px-3 py-2">
+                        <p>Loading data ....</p>
+                    </div>
+                    :
+                    <>
+                        <div className="flex flew-row flex-wrap w-full justify-center">
+                            {surah.filter((show: any)=>{
+                                if(query == ''){
+                                    return show;
+                                }else if(show.nama_latin.toLowerCase().includes(query.replace(/\s+/g,'-').toLocaleLowerCase())){
+                                    return show;
+                                }
+                            }).slice(0, page).map((show: any)=>{
+                                return(
+                                    <div key={show.nomor} className="flex w-full py-3 px-3 mx-3 my-3 bg-slate-200 rounded w-full h-fit md:w-1/4 shadow shadow-lg">
+                                        <button>
+                                            <Link href={`/baca-surat-ke/${show.nomor}`} passHref>
+                                                <div>
+                                                    <div className="flex flex-row hover:text-gray-500">
+                                                        <p className="text-lg rounded-full mx-4 my-3 px-2 py-2">
+                                                            {show.nomor}
+                                                        </p>
+                                                        <div className="flex-col my-3">
+                                                            <p className="font-bold italic">{show.nama_latin}</p>
+                                                            <p className="italic">{show.arti}, {show.jumlah_ayat} Ayat</p>
+                                                        </div>
                                                     </div>
                                                 </div>
-                                            </div>
-                                        </Link>
-                                    </button>
-                                </div>
-                            )
-                        })}
-                    </div>
-                    {pagination? 
-                    <>
-                        <p className="text-center text-lg">Tidak ada lagi data yang dapat ditampilkan!</p>
-                    </>
-                    :
-                        <div className="text-center">
-                            <button className="text-lg font-bold px-3 rounded-full outline outline-offset-2 outline-1 w-max my-5" onClick={Pagination}>
-                                Muat lebih banyak  
-                            </button>
+                                            </Link>
+                                        </button>
+                                    </div>
+                                )
+                            })}
                         </div>
-                    
+                        {pagination? 
+                        <>
+                            <p className="text-center text-lg">Tidak ada lagi data yang dapat ditampilkan!</p>
+                        </>
+                        :
+                            <div className="text-center">
+                                <button className="text-lg font-bold px-3 rounded-full outline outline-offset-2 outline-1 w-max my-5" onClick={Pagination}>
+                                    Muat lebih banyak  
+                                </button>
+                            </div>
+                        
+                        }
+                    </>
                     }
-                 </>
-                }
-                <p className="text-md text-center">&copy;2022 - Aditya build with ❤️</p>
+                    <p className="text-md text-center">&copy;2022 - Aditya build with ❤️</p>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
